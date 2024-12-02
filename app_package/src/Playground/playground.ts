9abf9081e90d09c9378860d477dbe588f56e9fc4
reddsaut <osauter@macalester.edu>
2024-12-02 11:08:45 +0100

    big triangle secretly little triangles

--- app_package/src/Playground/playground.ts
+++ app_package/src/Playground/playground.ts
@@ -5,12 +5,27 @@ class Playground {
     public static CreateScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement): BABYLON.Scene {
         function Grass(height: number, scene: BABYLON.Scene) {
             let width = 0.5;
-            let indices = [0,1,2];
-            let positions = [
-                0,0,0,
-                width,0,0,
-                width / 2,height,0
+            let indices = [
+                0, 1, 2,
+                1, 3, 2,
+                2, 3, 4,
+                3, 5, 4,
+                4, 5, 6,
+                5, 7, 6,
+                6, 7, 8
             ];
+            let positions = [
+                0, 0, 0,
+                width, 0, 0,
+                width/8, height/4, 0,
+                7*width/8, height/4, 0,
+                width/4, height/2, 0,
+                3*width/4, height/2, 0,
+                3*width/8, 3*height/4, 0,
+                5*width/8, 3*height/4, 0,
+                width/2, height, 0
+            ];
+            
             let normals: number[] = [];
             BABYLON.VertexData.ComputeNormals(positions, indices, normals);
 
